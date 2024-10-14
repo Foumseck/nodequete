b5336b549382df009f4d02da9350da0d5f920de1
Ludovic Pouillie <ludovicpouillie@gmail.com>
2024-10-14 17:49:11 +0200

    la vache

---
+++ index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import cowsay from "cowsay";
+
+
+dotenv.config();
+
+console.log(
+	cowsay.say({
+		text: `Bonjour, je suis ${process.env.NAME} j'étudie avec la promo ${process.env.CAMPUS}`,
+		e: "oO",
+		T: "U",
+	}),
+);
+
